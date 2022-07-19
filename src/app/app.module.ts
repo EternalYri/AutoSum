ee34d4ce51d9fafbe3b21791eb487881387f2561
EternalYri <97452847+EternalYri@users.noreply.github.com>
2022-07-19 15:57:57 +0300

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
 import { WeatherComponent } from './weather/weather.component';
 import { CardComponent } from './card/card.component';
 import { ButtonComponent } from './button/button.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +21,8 @@ import { ButtonComponent } from './button/button.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AddweatherService } from '../addweather.service'
 
 @Component({
   selector: 'app-weather',
@@ -7,7 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  city = ''
+  icon: any;
+  name: string = '';
+  arrTemp = [];
+  arrTime = [];
+  arrWeathers = [];
+  arrWind = [];
+  arrIcon = [];
+  obj = [];
+  end = [];
+
+  constructor(public http: AddweatherService) {}
+
+  onAdd(event: any) {
+    this.city = this.http.input[0].firstChild.value;
+    this.http.ngAdd(event)
+    .subscribe((data)=>{
+      this.obj = [];
+      this.arrIcon = [];
+      this.end = [];
+      this.arrTemp = [];
+      this.arrTime = [];
+      this.arrWeathers = [];
+      this.arrWind = [];
+
+      for (var i=0; i<data.length; i+=8){
+        this.arrTemp.push((data[i]['main']['temp']).toFixed(0))
+        this.arrWeathers.push(data[i]['weather']);
+        this.arrWind.push(data[i]['wind']['speed']);
+        this.arrTime.push(data[i]['dt_txt'])
+      }
+      for (let j=0; j<this.arrWeathers.length; j++) {
+        this.arrIcon.push(this.arrWeathers[j][0]['icon'])
+      }
+      for(let i =0; i<this.arrTemp.length; i++)
+      this.obj[i]= this.end.concat(this.arrTime[i], this.arrIcon[i], this.arrWind[i], this.arrTemp[i])
+      console.log(this.obj)
+    })
+    document.getElementsByClassName('city')[0].innerHTML = '';
+    document.getElementsByClassName('add')[0].innerHTML = '';
+  }
 
   ngOnInit(): void {
   }
